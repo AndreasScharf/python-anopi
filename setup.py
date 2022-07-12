20f9d493f19d93a4770f5d0a61701e74ea053a00
andreasscharf <andreas@r-sch.de>
2022-07-12 16:40:15 +0200

    rename

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='python-anopi',
+    name='python_anopi',
     version='0.1.0',
     description='A example Python package',
     url='https://github.com/AndreasScharf/python-anopi',
     author='Andreas Scharf',
     author_email='info@frappgmbh.de',
     license='MIT',
-    packages=['python-anopi'],
+    packages=['python_anopi'],
     install_requires=['pi-ina219'],
 
     classifiers=[
